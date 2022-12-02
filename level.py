a174f801f58d1455dc9ef3d4e0e797a81b70142f
Egororo <egorsemirenko04@gmail.com>
2022-12-02 22:57:02 +0300

    Добавлен полноэкранный режим

--- level.py
+++ level.py
@@ -2,14 +2,30 @@ import pygame as pg
 import json
 
 pg.init()
-width, height = 1920//2, 1080//2
-screen = pg.display.set_mode((width, height))
 
-platforms = []
+width, height = 1920//2, 1080//2
+sys_width, sys_height = pg.display.Info().current_w, pg.display.Info().current_h
+scales = (sys_width/width, sys_height/height)
+screen = pg.display.set_mode((sys_width, sys_height))
+
 WHITE = (200, 200, 200)
 CYAN = (0, 150, 150)
 BLACK = (0, 0, 0)
 BROWN = (100, 50, 20)
+
+platforms = []
+spikes = []
+
+
+def scale_objects(objects, scales):
+    scale_x, scale_y = scales
+
+    for object in objects:
+        object.x *= scale_x
+        object.xx *= scale_x
+        object.y *= scale_y
+        object.yy *= scale_y
+
 
 class Platform:
     def __init__(self, screen, x, y, xx, yy):
@@ -27,20 +43,17 @@ class Platform:
         self.x -= 0.1
 
 
-
-spikes = []
-
-
 class Spike:
     def __init__(self, screen, x, y, a, color=CYAN):
         self.x = x
         self.y = y
-        self.a = 25
+        self.xx = 25
+        self.yy = 25
         self.screen = screen
         self.color = color
 
     def draw(self):
-        pg.draw.rect(self.screen, self.color, (self.x, self.y, self.a, self.a))
+        pg.draw.rect(self.screen, self.color, (self.x, self.y, self.xx, self.yy))
 
     def move(self):
         self.x -= 0.1
@@ -62,13 +75,14 @@ def check_passage():
     return False
 
 
+scale_objects(platforms, scales)
+scale_objects(spikes, scales)
+
 fps = 60
 fpsClock = pg.time.Clock()
-
 finished = False
 
 while not finished:
-
     screen.fill(WHITE)
     for event in pg.event.get():
         if event.type == pg.QUIT:
