81453466db3237635dfed2a13bf472ba6d95d4c5
Mmvbr <vormax3@gmail.com>
2022-12-02 11:25:12 +0300

    12

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import models.menu as menu
+#import models.menu as menu
 import sys
 import pygame as pg
-
+import player
+width, height = 640, 480
+screen = pg.display.set_mode((width, height))
+hero=player.Player(100,100,screen)
 fps = 60
 fpsClock = pg.time.Clock()
 pg.init()
-
+hero.draw(screen)
 while True:
-    menu.theme()
+    #menu.theme()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
 
-    for object in menu.objects:
-        object.process()
+    #for object in menu.objects:
+     #   object.process()
 
     pg.display.flip()
     fpsClock.tick(fps)
