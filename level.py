d225eb26d4bc81190f50ee6ae9bf199836712e79
Egororo <egorsemirenko04@gmail.com>
2022-12-02 21:42:49 +0300

    Добавлены шипы в файл

--- level.py
+++ level.py
@@ -30,49 +30,27 @@ spikes = []
 
 
 class Spike:
-    def __init__(self, screen, x, y, color):
+    def __init__(self, screen, x, y, a, color=CYAN):
         self.x = x
         self.y = y
+        self.a = 25
         self.screen = screen
         self.color = color
 
     def draw(self):
-        pg.draw.rect(self.screen, self.color, (self.x, self.y, 25, 25))
+        pg.draw.rect(self.screen, self.color, (self.x, self.y, self.a, self.a))
 
 
-spikes.append(Spike(screen, 210, 190, CYAN))  # шип на левой верхней платформе
-spikes.append(Spike(screen, 210 + 25, 190, CYAN))  # шип на левой верхней платформе
-spikes.append(Spike(screen, 210 + 50, 190, CYAN))  # шип на левой верхней платформе
-
-spikes.append(Spike(screen, 600, 190, CYAN))  # шип на правой верхней платформе
-spikes.append(Spike(screen, 600 + 25, 190, CYAN))  # шип на правой верхней платформе
-
-spikes.append(Spike(screen, 369 - 25, height - 95, CYAN))  # шип внизу
-spikes.append(Spike(screen, 494 + 25, height - 95, CYAN))  # шип внизу
-
-for i in range(3):
-        spikes.append(Spike(screen, width//2 - 100 + 50 + 25*i, height//2, CYAN))  # шип в центре
-
-for i in range(5):
-    spikes.append(Spike(screen, 30, height//1.7 + 43+ 25*i, CYAN))  # шип слева внизу
-
-for i in range(6):
-    spikes.append(Spike(screen, width - 30 - 25, height//2.5 - 70 - 25*i, CYAN))  # шип справа сверху
-
-for i in range(5):
-    spikes.append(Spike(screen, width - 30 - 25, height//1.7 + 43+ 25*i, CYAN))  # шип справа внизу
-
-for i in range(3):
-    spikes.append(Spike(screen, 30, height // 2.5 - 65 - 25 * i, CYAN))  # шип слева сверху
-# добавить еще шипов, чтобы жизнь медом не казалась ;)
-
-
-with open('level.json', 'r') as file:
+with open('objects.json', 'r') as file:
     data = json.load(file)
-    N = len(data[0]['platform'])
-    for i in range(N):
+    count_of_platforms = len(data[0]['platform'])
+    for i in range(count_of_platforms):
         platform = Platform(screen, **data[0]['platform'][i])
         platforms.append(platform)
+    count_of_spikes = len(data[1]['spike'])
+    for i in range(count_of_spikes):
+        spike = Spike(screen, **data[1]['spike'][i])
+        spikes.append(spike)
 
 def check_passage():
     #FIXME: Условие при котором уровень будет пройден return True
@@ -96,21 +74,9 @@ while not finished:
     if check_passage():
         pass
         #FIXME: когда уровень пройден нужно открыть дверь
-    # with open('level.json', 'r') as file:
+    # with open('objects.json', 'r') as file:
     #     data = json.load(file)
     #     print(data)
     pg.display.update()
 
 pg.quit()
-#a = []
-# for el in platforms:
-#      a.append({'x':el.x1,'y':el.y1,'xx':el.x2,'yy':el.y2})
-# with open('level.json', 'w') as file:
-#     json.dump(a, file, indent=2)
-# print(a)
-with open('level.json', 'r') as file:
-    data = json.load(file)
-    print(data)
-    print(data[0])
-    print(data[0]['platform'])
-    print(data[0]['platform'][0])
