f7a5181188186e352e68457175946234a0d5ddc3
Egororo <egorsemirenko04@gmail.com>
2022-12-09 09:58:42 +0300

    убран лишний код

--- main.py
+++ main.py
@@ -100,38 +100,9 @@ while not finished:
     screen.fill(WHITE)
     theme.init_theme(screen)
     menu.start_buttons(screen, scales, game.start_game, menu.exit_game, menu.resume_game)
-    #for platform in platforms:
-        #platform.draw()
-    #for spike in spikes:
-        #spike.draw()
-    #for event in pg.event.get():
-     #   if event.type == pg.QUIT:
-      #      finished = True
-       # elif event.type==pg.KEYDOWN:
-        #    if event.key==pg.K_UP:
-         #       up =True
-          #  if event.key==pg.K_LEFT:
-           #     left =True
-            #if event.key==pg.K_RIGHT:
-             #   right =True
-           # if event.key==pg.K_SPACE:
-            #    space =True
-        #elif event.type==pg.KEYUP:
-         #   if event.key==pg.K_UP:
-          #      up=False
-           # if event.key==pg.K_LEFT:
-            #    left = False
-            #if event.key==pg.K_RIGHT:
-             #   right = False
-           # if event.key==pg.K_SPACE:
-            #    space = False
-    #hero.update(left,right,up,down,screen,platforms) 
     for event in pg.event.get():
                 if event.type == pg.QUIT:
                     finished = True
-                       
-    # print( pg.Rect.colliderect(hero.rect,platforms[3].make_rect()))
-    # print(hero.rect.x,hero.rect.y,hero.rect.width,hero.rect.heightd)
     pg.display.update()
     fpsClock.tick(fps)
 
