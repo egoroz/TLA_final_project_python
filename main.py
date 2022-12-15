748fc28cf31f7bc06225da50afdcb5d245ab397e
Mmvbr <vormax3@gmail.com>
2022-12-15 18:47:38 +0300

    нашел дверь

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ old_spikes = []
 old_buttons = []
 old_doors = []
 levels = 0
-
+collisable_obj=[]
 
 level.read_data(screen, platforms, spikes, buttons, doors, 'docs/objects.json')
+collisable_obj=platforms.copy()
+collisable_obj.append(doors[0])
 level.scale_objects(platforms, scales)
 level.scale_objects(spikes, scales)
 level.scale_objects(buttons, scales)
@@ -112,9 +114,12 @@ class Game:
                         self.right = False
                     if event.key==pg.K_SPACE:
                         self.space = False
-            hero.update(self.left, self.right, self.up, self.down, self.screen, platforms, spikes)
+            collisable_obj=platforms.copy()
+            collisable_obj.append(doors[0])
+            hero.update(self.left, self.right, self.up, self.down, self.screen, collisable_obj, spikes)
 
             levels, old_platforms, old_spikes, old_buttons, old_doors = level.update_level(screen, levels, hero, scales, platforms, spikes, buttons, doors, old_platforms, old_spikes, old_buttons, old_doors)
+            
             # print(platforms, old_spikes, old_buttons, old_doors)
             # print(levels)
             print(sys_width, sys_height)
